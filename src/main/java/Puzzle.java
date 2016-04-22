b6159a75b39ed4e1ea46070920691b2930c76d69
Lilo Onwuzu <lpr422@gmail.com>
2016-04-22 09:15:31 -0700

    GREEN for replacesA test

--- src/main/java/Puzzle.java
+++ src/main/java/Puzzle.java
@@ -5,14 +5,15 @@ public class Puzzle {
   public String runPuzzle(String phrase){
 
     String[] makeArray = phrase.split("");
-    String outputString = makeArray.toString();
+
+    for (Integer index = 0; index < makeArray.length; index++){
+      if (makeArray[index].equals("a")){
+        makeArray[index] = "-";
+      }
+    }
+
+    String outputString = String.join("", makeArray);
       return outputString;
 
   }
 }
-
-// for (index = 0; index > makeArray.length(); index++){
-//   if (makeArray[index].equals("a" || "e" || "i" || "o" || "u")){
-//     makeArray[index] = "-";
-//   }
-// }
