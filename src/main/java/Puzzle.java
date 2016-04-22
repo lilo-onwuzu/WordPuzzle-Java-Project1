1e401ad944a4e2681c180a4a882cc46166cebd03
Lilo Onwuzu <lpr422@gmail.com>
2016-04-22 09:31:17 -0700

    GREEN for acceptsUpperCaseCharactersForVowels

--- src/main/java/Puzzle.java
+++ src/main/java/Puzzle.java
@@ -4,7 +4,8 @@ public class Puzzle {
 
   public String runPuzzle(String phrase){
 
-    String[] makeArray = phrase.split("");
+    String newPhrase = phrase.toLowerCase();
+    String[] makeArray = newPhrase.split("");
 
     for (Integer index = 0; index < makeArray.length; index++){
       if (makeArray[index].equals("a") || makeArray[index].equals("e") || makeArray[index].equals("i") || makeArray[index].equals("o") || makeArray[index].equals("u")){
