18b0120be4dae7db3e7d7e9ced967e54bfd514ed
Lilo Onwuzu <lpr422@gmail.com>
2016-04-22 09:45:02 -0700

    GREEN for CodeRetainsUpperCaseCharactersForConsonants

--- src/main/java/Puzzle.java
+++ src/main/java/Puzzle.java
@@ -4,11 +4,10 @@ public class Puzzle {
 
   public String runPuzzle(String phrase){
 
-    String newPhrase = phrase.toLowerCase();
-    String[] makeArray = newPhrase.split("");
+    String[] makeArray = phrase.split("");
 
     for (Integer index = 0; index < makeArray.length; index++){
-      if (makeArray[index].equals("a") || makeArray[index].equals("e") || makeArray[index].equals("i") || makeArray[index].equals("o") || makeArray[index].equals("u")){
+      if (makeArray[index].equals("a")||makeArray[index].equals("A")||makeArray[index].equals("e")||makeArray[index].equals("E")||makeArray[index].equals("i")||makeArray[index].equals("I")||makeArray[index].equals("o")||makeArray[index].equals("O")||makeArray[index].equals("u")||makeArray[index].equals("U")){
         makeArray[index] = "-";
       }
     }
