5f1df9d86f83f68a74ce0f7b08f16757019527c8
Lilo Onwuzu <lpr422@gmail.com>
2016-04-22 10:31:53 -0700

    UI Build in process, home page done

--- src/main/java/Puzzle.java
+++ src/main/java/Puzzle.java
@@ -1,6 +1,21 @@
 import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class Puzzle {
+
+  public static void main(String[] args){
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
 
   public String runPuzzle(String phrase){
 
